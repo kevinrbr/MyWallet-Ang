f7d610ff1289be6ccb3fdcaf5669d03274464f2c
kevinrbr <kevinrbr16@gmail.com>
2023-02-12 19:00:02 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+   "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#C4A0FF',
+      'neutral': '#1D1C21',
+    }
+  },
+  plugins: [],
+}
